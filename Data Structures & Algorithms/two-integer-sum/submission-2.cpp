72193a2752638b35ea3f175fbd173a5656920c7e
AlnaElsa2004 <147088258+AlnaElsa2004@users.noreply.github.com>
2026-05-16 12:08:10 +0530

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int,int> mp;
+    int sum=target;
+    for(int i=0;i<nums.size();i++){
+        int complement=sum-nums[i];
+        if(mp.find(complement)!=mp.end()){
+            return{mp[complement],i};
+        }
+        mp[nums[i]]=i;
+    }
+    return {};
+    }
+};
