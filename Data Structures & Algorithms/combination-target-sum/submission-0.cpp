b1dc4a59f8d90aee62bb78dc8e4a9bbbdbc2a2a1
AlnaElsa2004 <147088258+AlnaElsa2004@users.noreply.github.com>
2026-06-09 10:59:30 +0530

    Add: combination-target-sum - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+set<vector<int>> s;
+    void combnsum(vector<int>& nums,vector<int> &comb,int i,vector<vector<int>> &res,int target){
+      if(target == 0){
+        if(s.find(comb)==s.end()){
+    res.push_back(comb);
+        s.insert(comb);}
+    return;
+}
+
+if(i == nums.size() || target < 0){
+    return;
+}
+        comb.push_back(nums[i]);
+        combnsum(nums,comb,i+1,res,target-nums[i]);
+        combnsum(nums,comb,i,res,target-nums[i]);
+        comb.pop_back();
+        combnsum(nums,comb,i+1,res,target);
+    }
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        vector<vector<int>> res;
+        vector<int> comb;
+        combnsum(nums,comb,0,res,target);
+        return res;
+    }
+};
